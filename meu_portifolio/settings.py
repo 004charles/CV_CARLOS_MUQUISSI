8f46395f78836a0cb9b6c0ebbfaad729b0352352
Carlos muquissi zangui <161896848+004charles@users.noreply.github.com>
2025-01-02 14:06:47 +0100

    Update settings.py

--- meu_portifolio/settings.py
+++ meu_portifolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-**ib08cgzftb74&cmjl3euu$vv+m$rwe2ss47@o$=xpmu4^1z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
