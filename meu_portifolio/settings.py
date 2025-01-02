d8e06c003730e10554b853baccc5d2d7758a42fd
Carlos Muquissi <muquissicarlos@gmail.com>
2025-01-02 20:00:52 +0100

    quarta

--- meu_portifolio/settings.py
+++ meu_portifolio/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-**ib08cgzftb74&cmjl3euu$vv+m$rwe2ss47@o$=xpmu4^1z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ["*"]
+DEBUG = True
+ALLOWED_HOSTS = []
 
 
 # Application definition
