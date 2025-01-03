3c06050a5a4d5f9a8c8a5db9fa7a9ca96ac47b1f
Carlos Muquissi <muquissicarlos@gmail.com>
2025-01-03 11:42:22 +0100

    sexta feira

--- meu_portifolio/settings.py
+++ meu_portifolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-**ib08cgzftb74&cmjl3euu$vv+m$rwe2ss47@o$=xpmu4^1z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
@@ -106,9 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Direção dos arquivos estáticos (CSS, JS)
 STATIC_URL = '/static/'
